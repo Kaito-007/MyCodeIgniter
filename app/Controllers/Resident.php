7969875b53837b5e2793f4c52357d2d2c93f2ae1
Kaito-007 <130149545+Kaito-007@users.noreply.github.com>
2023-04-07 19:18:25 +0800

    Add files via upload

---
+++ app/Controllers/Resident.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+class Resident extends BaseController
+{
+    public function index()
+    {
+        // Code to display a list of users and barangay residents
+    }
+
+    // Function For User
+    public function createUser()
+    {
+        // Code to display a form for creating a new user
+    }
+
+    public function updateUser()
+    {
+        // Code to handle the form for updating an existing user
+    }
+
+    public function deleteUser()
+    {
+        // Code to delete an existing user
+    }
+
+    // Function For Barangay Resident 
+    public function createBarangayResident()
+    {
+        // Code to display a form for creating a new barangay resident
+    }
+
+    public function updateBarangayResident()
+    {
+        // Code to handle the form for updating an existing barangay resident
+    }
+
+    public function deleteBarangayResident()
+    {
+        // Code to delete an existing barangay resident
+    }
+}
